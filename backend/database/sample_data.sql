26a297fb01e8491603dbb94f99c7fd81ca810472
IoannaDamianov <damianov.io@northeastern.edu>
2025-03-25 15:50:06 -0400

    added extra questions - Ioanna

--- backend/database/sample_data.sql
+++ backend/database/sample_data.sql
@@ -5,3 +5,9 @@ INSERT INTO questions (question, option_a, option_b, option_c, option_d, correct
 ("Which of these is not a programming language?", "Java", "Python", "Cobra", "HTML", "D"),
 ("What year was the first iPhone released?", "2005", "2006", "2007", "2008", "C");
 
+INSERT INTO questions (question, option_a, option_b, option_c, option_d, correct_answer) VALUES
+("Who wrote Romeo and Juliet?", "Charles Dickens", "J.K. Rowling", "William Shakespeare", "Mark Twain", "C"),
+("What is the largest ocean on Earth?", "Atlantic Ocean", "Pacific Ocean", "Indian Ocean", "Artic Ocean", "B"),
+("Who painted the Mona Lisa?", "Leonardo da Vinci", "Pablo Picasso", "Vincent van Gogh", "Michelangelo", "A"),
+("What is the smallest country in the world?", "Monaco", "Nauru", "San Marino", "Vatican City", "D"),
+("What is the tallest mountain in the world?", "Mount Everest", "Mount Olympus", "Kilimanjaro", "Mount Fuji", "A");
